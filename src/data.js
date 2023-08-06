f34986f4d5cd8705452ffd0cc962a015f21df82f
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-06 20:19:33 +0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,1 +1,15 @@
-
+const data = [
+	{
+		id: 1,
+		name: "Cox's Bazar",
+		info: "Cox's Bazar is a town on the southeast coast of Bangladesh. It's known for its very long, sandy beachfront, stretching from Sea Beach in the north to Kolatoli Beach in the south. Aggameda Khyang monastery is home to bronze statues and centuries-old Buddhist manuscripts.",
+		image: "https://media-cdn.tripadvisor.com/media/photo-c/1280x250/10/e2/f8/43/longest-sea-beach-in.jpg",
+		price: "8,758 BDT",
+	},
+	{
+		id: 2,
+		name: "Saint Martin",
+		info: `Saint Martin is part of the Leeward Islands in the Caribbean Sea. It comprises 2 separate countries, divided between its northern French side, called Saint-Martin, and its southern Dutch side, Sint Maarten. The island is home to busy resort beaches and secluded coves.`,
+		image: "https://images.unsplash.com/photo-1582650625119-3a31f8fa2699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FpbnQlMjBtYXJ0aW4lMjBpc2xhbmR8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+		price: "10,560 BDT",
+	},
