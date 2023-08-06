758784a4777ad73cfb65cbb5259622d0033a1400
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-06 20:19:52 +0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -13,3 +13,25 @@ const data = [
 		image: "https://images.unsplash.com/photo-1582650625119-3a31f8fa2699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FpbnQlMjBtYXJ0aW4lMjBpc2xhbmR8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
 		price: "10,560 BDT",
 	},
+
+	{
+		id: 3,
+		name: "Nijhum Dwip, Noakhali",
+		info: " Nijhum Dwip (Silent Island') is a small island under Hatiya Upazila of Noakhali District. It is a cluster of islands that emerged in the early 1950s. The main attraction is the herd of about 5000 cheetal or spotted deer, monkeys, a huge number of mated birds come in the winter season, enhancing the beauty of this island. Now this island has been declared as the unique eco-tourist spot.",
+		image: "https://prod-media-eng.dhakatribune.com/uploads/2020/08/caption-deers-grazing-in-nijhum-dwip-inundated-by-tidal-surges-and-incessant-rainfall-dhaka-tribune-1-1598622153735.jpg",
+		price: "4,695 BDT",
+	},
+	{
+		id: 4,
+		name: "Sundarbans",
+		info: "Sundarbans is a mangrove area in the delta formed by the confluence of the Ganges, Brahmaputra and Meghna Rivers in the Bay of Bengal. Sundarban Reserve Forest of Bangladesh is the largest mangrove forest in the world.",
+		image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Sundarban_Tiger.jpg/284px-Sundarban_Tiger.jpg",
+		price: "12,095 BDT",
+	},
+	{
+		id: 5,
+		name: "Rangamati",
+		info: "Rangamati is the administrative headquarter and town of Rangamati Hill District in the Chittagong Hill Tracts of Bangladesh. The town is located at 22°37'60N 92°12'0E and has an altitude of 14 metres. From Chittagong a 77 kilometres road leads to Rangamati.",
+		image: "https://t4.ftcdn.net/jpg/05/43/00/19/360_F_543001997_nYoMNochVF0DMhMImirztq1DovAUyMga.jpg",
+		price: "8,595 BDT",
+	},
