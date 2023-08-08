65f0aa20fa311c285afd37eed6177a98bc772faa
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-08 23:31:09 +0600

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,1 +1,11 @@
 
+import { useState } from "react";
+
+
+function Card({id, image, info, price, name, removeTour}) {
+    const[readmore, setReadmore] = useState(false);
+    const description = readmore ? info : `${info.substring(0, 200)}....`;
+
+    function readmoreHandler(){
+        setReadmore(!readmore);
+    }
