a863cf7a9ce5f0712dbe676bcad55f9944c842e2
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-08 23:30:39 +0600

    Update Tours.js

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -8,3 +8,15 @@ function Tours({tours, removeTour}) {
             <div>
                 <h2 className="title">Plan with Jihad</h2>
             </div>
+    <div className="cards">
+                {
+                    tours.map( (tour) => {
+                        return (
+                            <Card key={tour.id} {...tour} removeTour={removeTour}></Card>
+                        )
+                    } )
+                }
+            </div>
+        </div>
+    );
+}
