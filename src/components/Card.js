c58127a1c8eeb33d1774096ae9d7ecf87752eb18
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-08 23:31:49 +0600

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -18,3 +18,18 @@ function Card({id, image, info, price, name, removeTour}) {
                 <h4 className="tour-price">{price}</h4>
                 <h4 className="tour-name">{name}</h4>
             </div>
+    <div className="description">
+                {description}
+                <span className="read-more" onClick={readmoreHandler}>
+                    {readmore ? `Show less` : `Read more`}
+                </span>
+            </div>
+            </div>
+            
+            <button className="btn-red" onClick={ () => removeTour(id)}>
+                Not Interested
+            </button>
+
+        </div>
+    );
+}
