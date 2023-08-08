6ab17f858d2636f892934349d170c6f86df32e4a
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-08 23:29:12 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
 import React, { useState } from "react";
 import data from './data';
 import Tours from "./components/Tours";
+
+const App = () => {
+
+  const [tours, setTours] = useState(data);
+
+  function removeTour(id) {
+        const newTours = tours.filter(tour => tour.id !== id);
+        setTours(newTours);
+  }
+  if(tours.length === 0) {
+    return(
+        <div className="refresh">
+            <h2>No More Tours Left</h2>
+            <button className="btn-white" onClick={() => setTours(data)}>
+                Refresh
+            </button>
+        </div>
+    )
+}
