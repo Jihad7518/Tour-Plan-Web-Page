15276485d8d568fb85b9fc73bf8ae3b2675648a4
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-08 23:30:20 +0600

    Update Tours.js

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,1 +1,10 @@
 
+import React from "react";
+import Card from './Card';
+
+function Tours({tours, removeTour}) {
+    return (
+        <div className="container">
+            <div>
+                <h2 className="title">Plan with Jihad</h2>
+            </div>
